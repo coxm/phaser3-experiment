cd6cfab3d11535b84780492a708a7d57c3a56582
coxm <coxm@users.noreply.github.com>
2018-06-07 23:34:21 +0100

    Add platforms and player

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import {Game, AUTO} from 'phaser';
+import {AUTO, Game, Physics, Scene} from 'phaser';
 
 import './css/index.css';
 
@@ -7,6 +7,7 @@ const game = new Game({
   type: AUTO,
   width: 800,
   height: 600,
+  parent: 'root',
   physics: {
     default: 'arcade',
     arcade: {
@@ -21,15 +22,53 @@ const game = new Game({
 });
 
 
-function preload (this: Game): void {
+function preload (this: Scene): void {
+  this.load.image('sky', 'assets/sky.png');
+  this.load.image('ground', 'assets/platform.png');
+  this.load.image('star', 'assets/star.png');
+  this.load.image('bomb', 'assets/bomb.png');
+  this.load.spritesheet('dude', 'assets/dude.png', {
+    frameWidth: 32,
+    frameHeight: 48
+  });
 }
 
 
-function create(this: Game): void {
+let platforms: Physics.Arcade.StaticGroup;
+let player: Physics.Arcade.Sprite;
+function create(this: Scene): void {
+  this.add.image(400, 300, 'sky');
+  platforms = this.physics.add.staticGroup();
+  platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+  platforms.create(600, 400, 'ground');
+  platforms.create(50, 250, 'ground');
+  platforms.create(750, 220, 'ground');
+
+  player = this.physics.add.sprite(100, 450, 'dude');
+  player.setBounce(0.2);
+  player.setCollideWorldBounds(true);
+
+  this.anims.create({
+    key: 'left',
+    frames: this.anims.generateFrameNumbers('dude', {start: 0, end: 3}),
+    frameRate: 10,
+    repeat: -1,
+  });
+  this.anims.create({
+    key: 'turn',
+    frames: [{key: 'dude', frame: 4}],
+    frameRate: 20,
+  });
+  this.anims.create({
+    key: 'right',
+    frames: this.anims.generateFrameNumbers('dude', {start: 5, end: 8}),
+    frameRate: 10,
+    repeat: -1,
+  });
 }
 
 
-function update(this: Game): void {
+function update(this: Scene): void {
 }
 
 
