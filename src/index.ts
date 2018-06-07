4d2b834ad73b8fa4cac1dbddce5d526e53c58431
coxm <coxm@users.noreply.github.com>
2018-06-08 00:26:56 +0100

    Add stars

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import {AUTO, Game, Physics, Scene} from 'phaser';
+import {AUTO, Game, Math as PhaserMath, Physics, Scene} from 'phaser';
 
 import './css/index.css';
 
@@ -35,19 +35,23 @@ function preload (this: Scene): void {
 
 
 let platforms: Physics.Arcade.StaticGroup;
+let stars: Physics.Arcade.Group;
 let player: Physics.Arcade.Sprite;
+let cursors: CursorKeys;
 function create(this: Scene): void {
   this.add.image(400, 300, 'sky');
+
+  // Platforms.
   platforms = this.physics.add.staticGroup();
   platforms.create(400, 568, 'ground').setScale(2).refreshBody();
   platforms.create(600, 400, 'ground');
   platforms.create(50, 250, 'ground');
   platforms.create(750, 220, 'ground');
 
+  // Player.
   player = this.physics.add.sprite(100, 450, 'dude');
   player.setBounce(0.2);
   player.setCollideWorldBounds(true);
-
   this.anims.create({
     key: 'left',
     frames: this.anims.generateFrameNumbers('dude', {start: 0, end: 3}),
@@ -65,10 +69,50 @@ function create(this: Scene): void {
     frameRate: 10,
     repeat: -1,
   });
+  this.physics.add.collider(player, platforms);
+
+  // Stars.
+  stars = this.physics.add.group({
+    key: 'star',
+    repeat: 11,
+    setXY: {x: 12, y: 0, stepX: 70},
+  });
+  stars.children.iterate((child: Physics.Arcade.Sprite): void => {
+    child.setBounceY(PhaserMath.FloatBetween(0.4, 0.8));
+  }, undefined);
+  this.physics.add.collider(stars, platforms);
+  this.physics.add.overlap(player, stars, collectStar as any, undefined, this);
+
+  cursors = this.input.keyboard.createCursorKeys();
+
+  function collectStar(
+    player: Physics.Arcade.Sprite,
+    star: Physics.Arcade.Sprite
+  )
+    : void
+  {
+    star.disableBody(true, true);
+  }
 }
 
 
 function update(this: Scene): void {
+  if (cursors!.left!.isDown) {
+    player!.setVelocityX(-160);
+    player!.anims.play('left', true);
+  }
+  else if (cursors!.right!.isDown) {
+    player!.setVelocityX(160);
+    player!.anims.play('right', true);
+  }
+  else {
+    player!.setVelocityX(0);
+    player!.anims.play('turn');
+  }
+
+  if (cursors!.up!.isDown && player!.body.touching.down) {
+    player!.setVelocityY(-330);
+  }
 }
 
 
